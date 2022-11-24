13a2302476b5fa45739f54b749c74fd438689a69
Masum Billah <masum.monpura@gmail.com>
2022-11-24 19:25:35 +0600

    data loaded from mongodb

--- src/Pages/Home/HomeCategories/HomeCategories.js
+++ src/Pages/Home/HomeCategories/HomeCategories.js
@@ -1,9 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const HomeCategories = () => {
+  const { data: categories = [] } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/categories");
+      const data = await res.json();
+      return data;
+    },
+  });
   return (
     <div>
-      <h3>This is Home Categories</h3>
+      <h3>{categories.length}</h3>
     </div>
   );
 };
